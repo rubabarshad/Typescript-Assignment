8f19c75cb0b710fd1454d94e984fda82dbf548c8
Rubab Arshad <160515174+rubabarshad@users.noreply.github.com>
2024-06-08 08:16:52 +0500

    Delete Types.ts

--- Types.ts
+++
@@ -1,8 +0,0 @@
-let username: string = "Ruby";
-console.log(username);
-let age: number = 20;
-console.log(age);
-let generic_variable: any = true;
-console.log(generic_variable)
-let userlogin: boolean = false;
-console.log(userlogin);
