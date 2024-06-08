6cdbecabfda61f0e6d96ab8b836bf5a665b4b3ff
Rubab Arshad <160515174+rubabarshad@users.noreply.github.com>
2024-06-08 08:16:10 +0500

    Delete Types.js

--- Types.js
+++
@@ -1,8 +0,0 @@
-var username = "Ruby";
-console.log(username);
-var age = 20;
-console.log(age);
-var generic_variable = true;
-console.log(generic_variable);
-var userlogin = false;
-console.log(userlogin);
